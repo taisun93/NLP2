2740c86437583cfc6c35b33cf12184a8c7244d24
taisun93 <taisun93@gmail.com>
2022-09-17 14:23:13 -0500

    stuck

--- models.py
+++ models.py
@@ -46,20 +46,24 @@ class TrivialSentimentClassifier(SentimentClassifier):
 class FFNN(nn.Module):
     def __init__(self, inp, hid, out, embed: WordEmbeddings):
         super(FFNN, self).__init__()
-        self.embed = embed
-        
+        self.embeddings = embed
+        self.embedLayer = embed.get_initialized_embedding_layer()
 
         self.V = nn.Linear(inp, hid)
         # self.g = nn.Tanh()
         self.g = nn.ReLU()
-        self.W = nn.Linear(hid, out)
+        self.W = nn.Linear(hid, hid)
+        self.g1 = nn.ReLU()
+        self.W1 = nn.Linear(hid, hid)
+        self.g2 = nn.ReLU()
+        self.W2 = nn.Linear(hid, out)
         self.log_softmax = nn.LogSoftmax(dim=0)
         # Initialize weights according to a formula due to Xavier Glorot.
         nn.init.xavier_uniform_(self.V.weight)
         nn.init.xavier_uniform_(self.W.weight)
-        self.optimizer = optim.Adam(self.parameters(), lr=0.1)
+        
 
-    def forward(self, sentence):
+    def forward(self, sentence, printThis=False):
         """
         Runs the neural network on the given data and returns log probabilities of the various classes.
 
@@ -68,24 +72,42 @@ class FFNN(nn.Module):
         probabilities or a tuple of (loss, log probability) if you want to pass in y to this function as well
         """
 
-        self.zero_grad()
-        x = self.embed.get_embedding(sentence[0])
+        
+        # DAN
 
-        for word in sentence[1:]:
-            x += self.embed.get_embedding(word)
-
-        x /= len(sentence)
-        b = torch.from_numpy(x).float()
-        return self.log_softmax(self.W(self.g(self.V(b))))
+        input=[]
+        for word in sentence:
+            index = self.embeddings.word_indexer.index_of(word)
+            
+            if index == -1:
+                input.append(0)
+            else:
+                input.append(index)
+            # input += 
+        # print(len(input))
+        z = self.embedLayer(torch.LongTensor(input))
+        # print(z.size())
+        z = torch.mean(z, 0)
+        # if(printThis): print(z)
+        z = self.V(z)
+        z = self.g(z)
+        z = self.W(z)
+        # if(printThis): print("after hidden layer",z)
+        # z = self.g1(z)
+        # z = self.W1(z)
+        z = self.g2(z)
+        z = self.W2(z)
+        if(printThis): print("before softmax",z)
+        return self.log_softmax(z)
     
-    def back(self, output, target):
-        loss = nn.NLLLoss()
-        # print("output",output)
-        # print("target",target)
-        x = loss(output, target)
-        # print("loss",x)
-        x.backward()
-        self.optimizer.step()
+    # def back(self, output, target):
+    #     loss = nn.NLLLoss()
+    #     # print("output",output)
+    #     # print("target",target)
+    #     x = loss(output, target)
+    #     # print("loss",x)
+    #     x.backward()
+    #     self.optimizer.step()
 
 
 class NeuralSentimentClassifier(SentimentClassifier):
@@ -96,18 +118,11 @@ class NeuralSentimentClassifier(SentimentClassifier):
 
     def __init__(self, embed: WordEmbeddings):
         self.embed = embed
-        self.NN = FFNN(embed.get_embedding_length(), 50, 2, embed)
+        self.NN = FFNN(embed.get_embedding_length(), 50, 1, embed)
 
     def getprob(self, ex_words: List[str]) -> any:
         result = self.NN.forward(ex_words)
         return result
-
-    def correct(self, result, target):
-        # loss = torch.neg(result).dot(correct)
-        self.NN.back(result, target)
-        # return
-
-
 
     def predict(self, ex_words: List[str]) -> int:
         """
@@ -116,14 +131,12 @@ class NeuralSentimentClassifier(SentimentClassifier):
         :return: 0 or 1 with the label
         """
         # print(len(ex_words))
-        answer = self.getprob(ex_words).detach().numpy()
+        answer = self.NN.forward(ex_words, True)
         # print(answer[0])
-        # # print(answer[])
-        # print(answer)
-        if answer[0]>answer[1]:
-            return 0
-        # print("sdfljksdfjkldsfjlk")
-        return 1
+        
+        # if answer[0]>answer[1]:
+        #     return 0
+        # return 1
 
 
 def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_exs: List[SentimentExample], word_embeddings: WordEmbeddings) -> NeuralSentimentClassifier:
@@ -134,35 +147,41 @@ def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_ex
     :param word_embeddings: set of loaded word embeddings
     :return: A trained NeuralSentimentClassifier model
     """
-
-    # wordembeder = read_word_embeddings(args.word_vecs_path)
-    # print(word_embeddings.get_initialized_embedding_layer().embedding_dim)
+    lossFunction = nn.NLLLoss()
     classifier = NeuralSentimentClassifier(word_embeddings)
-    # zoop = train_exs[0].words[0]
-    # word_embeddings.get_embedding(zoop)
-    # torch.from_numpy(train_exs[0]).float()
-    for epoch in range(5):
+    optimizer = optim.Adam(classifier.NN.parameters(), lr=.1)
+    random.shuffle(train_exs)
+    train_exs = train_exs[:1000]
+    print(classifier.NN.parameters())
+    for epoch in range(3):
+        totalLoss = 0
         random.shuffle(train_exs)
         for ex in train_exs:
-            prob = classifier.getprob(ex.words)
-            right: Any
-            if ex.label == 0:
-                right = torch.tensor([1, 0])
-            else:
-                right = torch.tensor([0, 1])
+            words, target = ex.words, ex.label
+            prob = classifier.getprob(words)
+            
+            # correct: Any
+            # if ex.label == 0:
+            #     # print(0)
+            #     correct = torch.tensor([1, 0])
+            # else:
+            #     # print(1)
+            #     correct = torch.tensor([0, 1])
+            # # print(prob)
+            x = torch.LongTensor(target)
+            # print(x.size())
+            # print(prob.size())
 
-            classifier.correct(prob,right)
-        
-    # for ex in train_exs[:2]:
-    #     prob = classifier.getprob(ex.words)
-    #     correct: Any
-    #     if ex.label == 0:
-    #         correct = torch.tensor([1.0, 0.0])
-    #     else:
-    #         correct = torch.tensor([0.0, 1.0])
-
-    #     classifier.correct(prob,correct)
+            loss = lossFunction(prob,x)
+            # print(loss)
+            totalLoss+=loss
+            classifier.NN.zero_grad()
+            loss.backward()
+            optimizer.step()
+        print(f"loss in {epoch} is {totalLoss}")
+        print(classifier.predict(train_exs[0].words))
+        print(classifier.predict(train_exs[1].words))
     #     print(prob)
-    classifier.predict(train_exs[0].words)
+    # print(classifier.predict(train_exs[0].words))
 
     return classifier
--- neural_sentiment_classifier.py
+++ neural_sentiment_classifier.py
@@ -26,7 +26,7 @@ def _parse_args():
     parser.add_argument('--blind_test_path', type=str, default='data/test-blind.txt', help='path to blind test set (you should not need to modify)')
     parser.add_argument('--test_output_path', type=str, default='test-blind.output.txt', help='output path for test predictions')
     parser.add_argument('--no_run_on_test', dest='run_on_test', default=True, action='store_false', help='skip printing output on the test set')
-    parser.add_argument('--word_vecs_path', type=str, default='data/glove.6B.300d-relativized.txt', help='path to word embeddings to use')
+    parser.add_argument('--word_vecs_path', type=str, default='data/glove.6B.50d-relativized.txt', help='path to word embeddings to use')
     # Some common args have been pre-populated for you. Again, you can add more during development, but your code needs
     # to run with the default neural_sentiment_classifier for submission.
     parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
@@ -106,18 +106,18 @@ if __name__ == '__main__':
         model = train_deep_averaging_network(args, train_exs, dev_exs, word_embeddings)
     else:
         model = TrivialSentimentClassifier()
-    print("=====Train Accuracy=====")
-    train_acc, train_f1, train_out = evaluate(model, train_exs)
-    print("=====Dev Accuracy=====")
-    dev_acc, dev_f1, dev_out = evaluate(model, dev_exs)
-    train_eval_time = time.time() - start_time
-    print("Time for training and evaluation: %.2f seconds" % train_eval_time)
+    # print("=====Train Accuracy=====")
+    # train_acc, train_f1, train_out = evaluate(model, train_exs)
+    # print("=====Dev Accuracy=====")
+    # dev_acc, dev_f1, dev_out = evaluate(model, dev_exs)
+    # train_eval_time = time.time() - start_time
+    # print("Time for training and evaluation: %.2f seconds" % train_eval_time)
 
     # Write the test set output
-    if args.run_on_test:
-        test_exs_predicted = [SentimentExample(words, model.predict(words)) for words in test_exs]
-        write_sentiment_examples(test_exs_predicted, args.test_output_path)
+    # if args.run_on_test:
+    #     test_exs_predicted = [SentimentExample(words, model.predict(words)) for words in test_exs]
+    #     write_sentiment_examples(test_exs_predicted, args.test_output_path)
 
-    data = {'dev_acc': dev_acc, 'dev_f1': dev_f1, 'execution_time': train_eval_time, 'output': dev_out}
-    print("=====Results=====")
-    print(json.dumps(data, indent=2))
+    # data = {'dev_acc': dev_acc, 'dev_f1': dev_f1, 'execution_time': train_eval_time, 'output': dev_out}
+    # print("=====Results=====")
+    # print(json.dumps(data, indent=2))
